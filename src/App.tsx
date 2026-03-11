9ebd7d17fd54a6ae50fcc06d08ae21ebc491bf11
Komal Sathvik <mksathvik03@gmail.com>
2026-03-11 20:33:09 +0530

    added test comp

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,17 @@ import Community from "./components/Community";
 import FAQ from "./components/FAQ";
 import PacmanCodenyx from "./components/pacman/PacmanCodenyx";
 import Footer from "./components/Footer";
+import TestRedirect from "./components/TestRedirect";
 import useSmoothScroll from "./hooks/useSmoothScroll";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   useSmoothScroll();
+
+  if (window.location.pathname === '/cvr-p1') {
+    return <TestRedirect />;
+  }
 
   return (
     <div className="bg-black min-h-screen text-white font-body selection:bg-white selection:text-black">
